196435bf2c3f760e95ebe9fdacaa18cbfa4cfb59
sumeyyetuzuner17 <sumeyyedeniz34@gmail.com>
2023-02-09 23:10:29 +0300

    responsive... bitmedi henüz

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,22 +6,52 @@ const SCProjeler = styled.div`
   height: 800px;
   width: 1500px;
   margin-top: 2px;
+
+  @media (max-width: 550px) {    
+    height: 1400px;
+  }
+ 
 `;
 
 const SCProjeItem = styled.div`
   display: flex;
   align-self: center;
+
+  @media (max-width: 550px) {
+    flex-direction: column;  
+    
+  }
 `;
 
 const SCImg = styled.img`
   width: 265px;
   height: 250px;
+
+  @media (max-width: 550px) {
+    flex-direction: column;  
+    width: 205px;
+  height: 200px;
+  }
 `;
+
+const SCP=styled.p`
+ @media (max-width: 550px) {
+    flex-direction: column;  
+    font-size: small;
+    text-align: start;
+    width: 200px;
+  }
+
+`
+const SCMedia=styled.div`
+margin-left: "100px";
+`
+
 
 function Projects() {
   return (
     <SCProjeler className="projeler" id="projeler">
-      <div style={{ marginLeft: "200px", paddingTop: "100px" }}>
+      <div className="projeler1alti"  style={{ marginLeft: "200px", paddingTop: "100px" }}>
         <div
           className="baslik"
           style={{
@@ -44,7 +74,7 @@ function Projects() {
           </div>
           <div className="projeDesc" style={{ paddingLeft: "20px" }}>
             <p>WORKINTECH</p>
-            <p>
+            <SCP>
               Bu projede, sayıları herhangi bir sırayla toplayabilen, çarpabilen
               ve çıkartabilen,ayrıca
               <br />
@@ -56,7 +86,7 @@ function Projects() {
               hesap makinesi bildiklerimizden <br />
               biraz farklı. Her yeni hesaplamasında önceki hesabın sonucunu ve
               yeni bir sayıyı parametre olarak almaktadır.
-            </p>
+            </SCP>
             <div
               className="divler"
               style={{ display: "flex", padding: " 8px 18px 9px", gap: "10px" }}
@@ -129,7 +159,7 @@ function Projects() {
           </div>
           <div className="projeDesc" style={{ paddingLeft: "20px" }}>
             <p>WORKINTECH</p>
-            <p>
+            <SCP>
               Bu projede, sayıları herhangi bir sırayla toplayabilen, çarpabilen
               ve çıkartabilen,ayrıca
               <br />
@@ -141,7 +171,7 @@ function Projects() {
               hesap makinesi bildiklerimizden <br />
               biraz farklı. Her yeni hesaplamasında önceki hesabın sonucunu ve
               yeni bir sayıyı parametre olarak almaktadır.
-            </p>
+            </SCP>
             <div
               className="divler"
               style={{ display: "flex", padding: " 8px 18px 9px", gap: "10px" }}
